eb95aa02019046967d252e1618206ddbcc654f69
Brendon <brendon.lopes.21@gmail.com>
2023-03-25 16:22:09 -0300

    feat: add utils folder
    containing the array of links for the navbar.

---
+++ utils/navbar/navItems.ts
@@ -0,0 +1,23 @@
+interface INavItem {
+  id: number
+  name: string
+  href: string
+}
+
+export const navItems: INavItem[] = [
+  {
+    id: 1,
+    name: "Omni trafo",
+    href: "#",
+  },
+  {
+    id: 2,
+    name: "Sinalizador de faltas",
+    href: "#",
+  },
+  {
+    id: 3,
+    name: "Acoplador DP",
+    href: "#",
+  }
+]
