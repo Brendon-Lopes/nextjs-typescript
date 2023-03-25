af69c00cd1c0cf9096f64329d82b608df7570c06
Brendon <brendon.lopes.21@gmail.com>
2023-03-25 19:13:31 -0300

    chore: add util file
    to hold content to display on pages.
    e.g.: titles, descriptions...

---
+++ utils/infoSection/index.ts
@@ -0,0 +1,12 @@
+export const title = `
+Tenha Informações em tempo real das condições dos
+ativos e com o monitoramento inteligente
+`;
+
+export const description = `
+Tenha acesso a informações em tempo real, administração
+remota de dados importantes da rede elétrica como: tensão,
+corrente, potência passante, energia, temperatura, sobrecarga
+entre outros, mapeando seu funcionamento, prevendo a vida útil
+dos ativos e aferindo perdas técnicas e não técnicas.
+`;
