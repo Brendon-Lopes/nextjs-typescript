b3ef47848c7e92e2b8032b7a07fbb801741205d7
Brendon <brendon.lopes.21@gmail.com>
2023-03-25 16:28:04 -0300

    feat: finish Navbar for mobile

--- components/Navbar/index.tsx
+++ components/Navbar/index.tsx
@@ -1,25 +1,37 @@
+import { useState } from "react";
 import { NavbarWrapper } from "./styles";
 import Image from "next/image";
+import { navItems } from "@/utils/navbar/navItems";
 
 export default function Navbar() {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
   return (
     <NavbarWrapper>
       <section className="navbar-container">
         <Image className="logo" src="/hvex.svg" alt="Logo" width={123} height={57} />
-        <button className="toggle-menu-button">
+
+        <button className="toggle-menu-button" onClick={() => setIsMenuOpen(!isMenuOpen)}>
           <div className="toggle-menu-button__bar" aria-hidden="true"></div>
           <div className="toggle-menu-button__bar" aria-hidden="true"></div>
           <div className="toggle-menu-button__bar" aria-hidden="true"></div>
         </button>
-        <nav className="navbar">
+
+        <nav className={`navbar ${isMenuOpen ? '' : 'hidden'}`} aria-hidden={!isMenuOpen}>
           <ul className="navbar__list">
-            <li className="navbar__item">
-              <a className="navbar__link" href="#">
-                Omni trafo
-              </a>
+            {navItems.map((item) => (
+              <li key={item.id} className="navbar__item">
+                <a className="navbar__link" href="#">
+                  {item.name}
+                </a>
+              </li>
+            ))}
+            <li>
+              <button className="buy-now-button">Comprar agora</button>
             </li>
           </ul>
         </nav>
+
       </section>
     </NavbarWrapper>
   );
--- components/Navbar/styles.ts
+++ components/Navbar/styles.ts
@@ -1,7 +1,7 @@
 import styled from 'styled-components';
 
 export const NavbarWrapper = styled.header`
-  background-color: var(--navbar-background);
+  background-color: rgba(var(--navbar-background));
   display: flex;
   justify-content: center;
 
@@ -12,6 +12,7 @@ export const NavbarWrapper = styled.header`
     display: flex;
     justify-content: space-between;
     align-items: center;
+    box-shadow: 0 4px 4px rgba(0, 0, 0, 0.25);
   }
 
   .logo {
@@ -26,12 +27,58 @@ export const NavbarWrapper = styled.header`
     display: flex;
     flex-direction: column;
     justify-content: space-between;
+    z-index: 9999;
   }
 
   .toggle-menu-button__bar {
     width: 100%;
     height: 14%;
-    background-color: var(--white);
+    background-color: rgba(var(--white));
     border-radius: 0.2rem;
+  }
+
+  .navbar {
+    padding-top: 6.5rem;
+    padding-left: 1rem;
+    position: fixed;
+    right: 0;
+    top: 0;
+    height: 100vh;
+    width: 60%;
+    transition: all 0.2s ease-in-out;
+    /* From https://css.glass */
+    background: rgba(var(--navbar-background), 0.8);
+    backdrop-filter: blur(7px);
+    -webkit-backdrop-filter: blur(7px);
+    border-left: 1px solid rgba(var(--white), 0.37);
+  }
+
+  .navbar__list {
+    display: flex;
+    flex-direction: column;
+  }
+
+  .navbar__item {
+    padding: 0.5rem 0;
+    font-size: 1.2rem;
+    font-weight: 500;
+  }
+
+  .navbar__item:first-child {
+    color: rgba(var(--accent-primary));
+  }
+
+  .hidden {
+    transform: translateX(100%);
+  }
+
+  .buy-now-button {
+    font-size: 1.2rem;
+    border: 1px solid rgba(var(--accent-primary));
+    color: rgba(var(--accent-primary));
+    border-radius: 0.3rem;
+    margin-top: 1rem;
+    padding: 0.85rem 2rem;
+    background: none;
   }
 `;
