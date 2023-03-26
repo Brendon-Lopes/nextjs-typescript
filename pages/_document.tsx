2380f62f4d342590a00fa7d168aafe4243dbe1fc
Brendon <brendon.lopes.21@gmail.com>
2023-03-26 16:08:43 -0300

    chore: improve SEO by adding a meta description

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -2,8 +2,18 @@ import { Html, Head, Main, NextScript } from 'next/document'
 
 export default function Document() {
   return (
-    <Html lang="en">
-      <Head />
+    <Html lang="pt-BR">
+      <Head>
+        <meta charSet="utf-8" />
+        <meta
+          name="description"
+          content="Conheça a HVEX: Somos única fabricante
+            de equipamentos para laboratório de alta tensão,
+            Unimos essas duas expertises com intuito de oferecer
+            tecnologias incorporadas às áreas de smart city e smart grid,
+            expandindo inovação e análise de dados para o setor elétrico."
+        />
+      </Head>
       <body>
         <Main />
         <NextScript />
