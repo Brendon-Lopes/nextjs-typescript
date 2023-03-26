fd5f540cfc5679d28d283b9f1d3cdb93d948014c
Brendon <brendon.lopes.21@gmail.com>
2023-03-26 00:20:21 -0300

    chore: add util folder for welcome page section.
    containing info and text to display.

---
+++ utils/welcomeSection/index.ts
@@ -0,0 +1,34 @@
+export const title = `
+Seja bem-vindo a era da energia inteligente
+`;
+
+export const description = `
+As redes inteligentes são um caminho sem volta,
+o desenvolvimento e disceminação de dispositivos
+de medição e monitoramento são as ferramentas para
+redução de custos, perdas e trazem mais previsibilidade
+para as utilities de energia.
+`;
+
+export const items = [
+  {
+    id: 1,
+    text: "Monitoramento inteligente de ativos",
+  },
+  {
+    id: 2,
+    text: "Melhoria dos indicadores de continuidade",
+  },
+  {
+    id: 3,
+    text: "Comunicação Modular",
+  },
+  {
+    id: 4,
+    text: "Business Inteligence",
+  },
+  {
+    id: 5,
+    text: "Redução de Perdas técnicas e Não técnicas",
+  }
+]
