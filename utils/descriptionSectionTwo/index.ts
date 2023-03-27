a407da643d3e159c54e376332d3117d684eb6caf
Brendon <brendon.lopes.21@gmail.com>
2023-03-27 16:28:58 -0300

    chore: add info to use in description section two

---
+++ utils/descriptionSectionTwo/index.ts
@@ -0,0 +1,55 @@
+export const title = 'OMNI TRAFO';
+
+export const subtitle = `
+Monitoramento inteligente de transformadores de distribuição
+`;
+
+export const description = `
+O Omni trafo é uma solução consolidada para monitoramento de ativos.
+Através deste equipamento, informações importantes da rede são
+enviadas em tempo real, permitindo, assim, a administração remota
+de dados como:
+`;
+
+export const descriptionCards = [
+  {
+    id: 1,
+    caption: 'Monitoramento de  tensão, corrente e carga',
+    src: '/descriptionCardsCircle/monitoramento.svg',
+  },
+  {
+    id: 2,
+    caption: 'Análise de perda de vida útil do transformador',
+    src: '/descriptionCardsCircle/analise-perda.svg',
+  },
+  {
+    id: 3,
+    caption: 'Medição do balanço energético de Perdas',
+    src: '/descriptionCardsCircle/medicao-balanco.svg',
+  },
+  {
+    id: 4,
+    caption: 'Detecção de faltas na rede elétrica',
+    src: '/descriptionCardsCircle/deteccao.svg',
+  },
+  {
+    id: 5,
+    caption: 'Análise de Fator de potência',
+    src: '/descriptionCardsCircle/analise-fator.svg',
+  },
+  {
+    id: 6,
+    caption: 'Harmônicas de corrente e tensão',
+    src: '/descriptionCardsCircle/harmonicas.svg',
+  },
+  {
+    id: 7,
+    caption: 'Medição de temperatura interna',
+    src: '/descriptionCardsCircle/medicao-temperatura.svg',
+  },
+  {
+    id: 8,
+    caption: 'Supervisão de baixa tensão',
+    src: '/descriptionCardsCircle/supervisao.svg',
+  },
+];
