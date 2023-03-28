2ae9518dfbe6645ec8adcacc84e5c148ea3a4245
Brendon <brendon.lopes.21@gmail.com>
2023-03-28 11:43:03 -0300

    style: fix style from transparent button component
    remove margin top, making the component more flexible and customizable.

--- components/TransparentButton/styles.ts
+++ components/TransparentButton/styles.ts
@@ -1,5 +1,4 @@
 import styled from 'styled-components';
-import { media } from '@/utils/styles/breakpoints';
 
 export const TransparentButtonWrapper = styled.button`
   font-size: 1rem;
@@ -7,12 +6,7 @@ export const TransparentButtonWrapper = styled.button`
   border: 1px solid rgba(var(--accent-primary));
   color: rgba(var(--accent-primary));
   border-radius: 0.3rem;
-  margin-top: 1rem;
   padding: 0.85rem 2rem;
   background: none;
   cursor: pointer;
-
-  ${media.desktop} {
-    margin-top: 0;
-  }
 `;
