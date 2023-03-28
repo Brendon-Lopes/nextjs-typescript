bbfb4ecdfb21d513b8ebf9c25f4c5ce65dd16ebf
Brendon <brendon.lopes.21@gmail.com>
2023-03-28 16:43:25 -0300

    chore: add info to add on the footer
    add social networks images and text content for the footer.

---
+++ utils/footer/index.ts
@@ -0,0 +1,36 @@
+export const address = {
+  title: 'Endereço',
+  content: 'R. Ismael Pinto de Noronha, 86 - Nossa Sra. de Fatima, Itajubá - MG, 37502-508',
+};
+
+export const contact = {
+  title: 'Contato',
+  number: '(35) 3622-2699',
+  email: 'contato@hvex.com.br',
+};
+
+export const social = {
+  title: 'Confira nossas redes',
+  items: [
+    {
+      id: 1,
+      alt: 'linkedin',
+      src: '/socials/linkedin.svg',
+    },
+    {
+      id: 2,
+      alt: 'facebook',
+      src: '/socials/facebook.svg',
+    },
+    {
+      id: 3,
+      alt: 'instagram',
+      src: '/socials/instagram.svg',
+    },
+    {
+      id: 4,
+      alt: 'youtube',
+      src: '/socials/youtube.svg',
+    },
+  ],
+};
