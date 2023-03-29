c3ebf977268b1a1a0894804d59dd18b620ae8b30
Brendon <brendon.lopes.21@gmail.com>
2023-03-29 19:02:48 -0300

    chore: add test ids for testing purposes

--- components/Footer/index.tsx
+++ components/Footer/index.tsx
@@ -9,23 +9,23 @@ export default function Footer() {
         <section className="footer-main">
           <Image className="logo" src="/hvex.svg" width={123} height={57} alt="Logo" />
 
-          <section className="footer-info">
+          <section data-testid="footer__address-section" className="footer-info">
             <h4>{content.address.title}</h4>
             <p className="address">{content.address.content}</p>
           </section>
 
-          <section className="footer-info">
+          <section data-testid="footer__contact-section" className="footer-info">
             <h4>{content.contact.title}</h4>
             <p>{content.contact.number}</p>
             <p>{content.contact.email}</p>
           </section>
 
-          <section className="socials-container">
+          <section data-testid="footer__socials-section" className="socials-container">
             <h4>{content.social.title}</h4>
             <section className="socials">
               {content.social.items.map(({ id, src, alt }) => (
                 <div key={id} className="social-icon">
-                  <Image src={src} width={24} height={24} alt={alt} />
+                  <Image src={src} width={24} height={24} alt={alt} data-testid={`footer__social-icon-${id}`} />
                 </div>
               ))}
             </section>
@@ -34,7 +34,7 @@ export default function Footer() {
 
         <hr />
 
-        <section className="policies-section">
+        <section data-testid="footer__policies-section" className="policies-section">
           <p>{content.policies.rights}</p>
           <section className="buttons-section">
             <button className="policy-button">{content.policies.privacy}</button>
