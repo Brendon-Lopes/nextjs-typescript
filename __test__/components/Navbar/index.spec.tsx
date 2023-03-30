daca987ceb383b59202c704a48477063c19be24a
Brendon <brendon.lopes.21@gmail.com>
2023-03-30 00:14:03 -0300

    test: add more tests to Navbar component
    test if menu toggle is working properly on mobile.

--- __test__/components/Navbar/index.spec.tsx
+++ __test__/components/Navbar/index.spec.tsx
@@ -1,4 +1,4 @@
-import { render, screen } from '@testing-library/react';
+import { fireEvent, render, screen } from '@testing-library/react';
 import '@testing-library/jest-dom';
 import { Navbar } from '@/components';
 import { navItems } from '@/utils/navbar/navItems';
@@ -11,10 +11,31 @@ describe('Navbar Component', () => {
 
     navItems.forEach(({ name }) => {
       expect(screen.getByText(name)).toBeInTheDocument()
-    })
+    });
 
     expect(screen.getByRole('button', { name: /abrir menu/i })).toBeInTheDocument()
 
     expect(screen.getByRole('button', { name: /comprar agora/i })).toBeInTheDocument()
-  })
-})
+  });
+
+  describe('On mobile', () => {
+    it('should toggle the menu', () => {
+      window.innerWidth = 375;
+      window.innerHeight = 667;
+
+      render(<Navbar />)
+
+      const menuButton = screen.getByRole('button', { name: /abrir menu/i })
+
+      expect(screen.getByRole('navigation')).toHaveClass('hidden')
+
+      fireEvent.click(menuButton)
+
+      expect(screen.getByRole('navigation')).not.toHaveClass('hidden')
+
+      fireEvent.click(menuButton)
+
+      expect(screen.getByRole('navigation')).toHaveClass('hidden')
+    });
+  });
+});
